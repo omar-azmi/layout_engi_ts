12b239d7c69c8b0379a6b4c0b4bcba4430145129
Omar Azmi <64020006+omar-azmi@users.noreply.github.com>
2023-10-18 09:07:26 -0400

    in `framesplit.ts`, made the following changes: - the unit of length now also includes raw numbers and raw numeric strings, both of which et interpreted as just pixels. - modify `parseLengthUnit` and `stringifyLengthUnit` to take care of pure numbers and numeric string. - add `FrameSplit.hit` method, which can perform a deep-nested hit test - modify `FrameSplit.toPreview` so that parent frame can now get colored if a non-undefined `color` parameter is provided - modify `FrameSplit.toPreview` so that "free space child" frame adopts the color specified in the parameter, otherwise it defaults to "gray" `framesplit.test.ts` now contains a an example that can select framesplit via mouse clicks and with event signal debouncing `framesplit.test2.ts` is an extension to `framesplit.test.ts`, which allows for changing the framsplit dimensions and margins by first selecting with mouse click, and then using the input on the table that pops up update dependencies: - `kitchensink_ts`: `v0.7.0` -> `v0.7.1` make `build_dist.ts` more capable of tree shaking (dead code elimination) by utilizing two build step: 1) bundle 2) minify

--- build_dist.ts
+++ build_dist.ts
@@ -1,4 +1,4 @@
-import { build as esbuild, stop as esstop } from "https://deno.land/x/esbuild@v0.17.19/mod.js"
+import { build as esbuild, stop as esstop, transform as estransform } from "https://deno.land/x/esbuild@v0.17.19/mod.js"
 import { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.8.1/mod.ts"
 
 /** use:
@@ -6,23 +6,38 @@ import { denoPlugins } from "https://deno.land/x/esbuild_deno_loader@0.8.1/mod.t
  * - `"./examples/${num}/${script}.ts"` for compiling an example
 */
 const
-	compile_file = Deno.args[0] ?? "./src/framesplit.test.ts",
-	compiled_file = `./dist/${compile_file.split("/").reverse()[0].slice(0, -3)}.js`
+	compile_file = Deno.args[0] ?? "./src/framesplit.test2.ts",
+	outdir = "./dist/",
+	compiled_file = `${outdir}${compile_file.split("/").reverse()[0].slice(0, -3)}.js`
+
+await Deno.mkdir(outdir, { recursive: true })
 let t0 = performance.now(), t1: number
-await esbuild({
+const bundled_code = await esbuild({
 	entryPoints: [compile_file],
-	outdir: "./dist/",
+	outdir: outdir,
 	bundle: true,
-	//minify: true,
-	treeShaking: true,
+	minifySyntax: true,
 	platform: "neutral",
 	format: "esm",
 	target: "esnext",
 	plugins: [...denoPlugins()],
+	write: false,
 	define: {},
 	//mangleProps: /\$$/
 })
+
+await Promise.all(bundled_code.outputFiles.map(
+	async ({ text, path }, file_number) => {
+		await Deno.writeTextFile(path, (await estransform(text, {
+			minify: true,
+			platform: "browser",
+			format: "esm",
+			target: "esnext",
+		})).code, { create: true })
+		console.log("file ", file_number, " binary size:", (await Deno.stat(path))?.size / 1024, "kb")
+	}
+))
+
 esstop()
 t1 = performance.now()
 console.log("execution time:", t1 - t0, "ms")
-console.log("dist binary size:", Deno.statSync(compiled_file).size / 1024, "kb")
